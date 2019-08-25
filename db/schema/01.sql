dcc6d51cbb9d518b2662d052a1e84f75906883af
tylerccaceres <tyler.caceres@gmail.com>
2019-08-24 22:12:19 -0400

    finalize, remove express information

--- db/schema/01.sql
+++ db/schema/01.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
   id serial PRIMARY KEY,
-  first_name varchar(50) NOT NULL,
+  first_name varchar(50),
   last_name varchar(50)
 );
--- index.js
+++ index.js
@@ -1,80 +1,56 @@
 // CONSTANTS ********************
-const {PORT, DATABASE, USERNAME, PASSWORD} = require('./data/constants');
-
-// IMPORTS ********************
-const express = require('express');
-const app = express();
+const {DATABASE, USERNAME, PASSWORD} = require('./data/constants');
 
 // DATABASE ********************
 const Sequelize = require('sequelize');
 
+// connection between sequelizer and postgreSQL
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
   define: {timestamps: false}
 });
 
-// APP INITIALIZATION ********************
-// app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch((err) => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
+// User schema for users table
 const User = sequelize.define('user', {
   first_name: {
-    type: Sequelize.STRING,
-    allowNull: false
+    type: Sequelize.STRING
+    // allowNull: false
   },
   last_name: {
     type: Sequelize.STRING
   }
 });
 
-// // Find all users
-// User.findAll().then((users) => {
-//   console.log('All users:', JSON.stringify(users, null, 4));
-// });
+// Find all users
+User.findAll().then((users) => {
+  console.log('All users:', JSON.stringify(users, null, 4));
+});
 
 // Create a new user
 User.create({first_name: 'Tyler', last_name: 'Caceres'})
-  .then((res) => {
-    console.log('Created entry:', res);
+  .then((user) => {
+    console.log('Created entry:', JSON.stringify(user, null, 4));
   })
   .catch((err) => console.log(err));
 
-// // Delete everyone named "Jane"
-// User.destroy({
-//   where: {
-//     first_name: 'Jane'
-//   }
-// }).then(() => {
-//   console.log('Done');
-// });
+// BulkCreate new users
+User.bulkCreate([
+  {first_name: 'Tyler V2', last_name: 'Caceres V2'},
+  {first_name: 'Tyler V3', last_name: 'Caceres V3'}
+]).then((user) => console.log('Created entry:', JSON.stringify(user, null, 4)));
 
-// // Change everyone without a last name to "Doe"
-// User.update(
-//   {last_name: 'Doe'},
-//   {
-//     where: {
-//       last_name: null
-//     }
-//   }
-// ).then(() => {
-//   console.log('Done');
-// });
+// Delete everyone named "Ash"
+User.destroy({
+  where: {first_name: 'ash'}
+}).then(() => console.log('Entry deleted.'));
 
-// SERVER INITIALIZATION********************
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// Change everyone with first name 'gary' to 'gerald'
+User.update(
+  {first_name: 'gerald'},
+  {
+    where: {
+      first_name: 'gary'
+    }
+  }
+).then(() => console.log('User updated.'));
